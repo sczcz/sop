b804ce6e43efd088db1bd9b50c2be500cc017b0c
Johannes Danielson <hannes16@live.se>
2023-04-12 13:14:35 +0200

    Implemented thymeleaf Added html testfile

---
+++ src/main/java/com/sparaochpara/sop/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.sparaochpara.sop.controller;
+
+import com.sparaochpara.sop.dto.UserDto;
+import com.sparaochpara.sop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model){
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users-list";
+    }
+
+}
