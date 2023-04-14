dda3155483f7d6497b92680c7aa0be9fb74eeceb
sczcz <funkheim@hotmail.com>
2023-04-14 13:39:09 +0200

    Removed @SpringBootTest annotation for deployment

--- src/test/java/com/sparaochpara/sop/SopApplicationTests.java
+++ src/test/java/com/sparaochpara/sop/SopApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparaochpara.sop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SopApplicationTests {
 
 	@Test
