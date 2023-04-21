56bbc1053d4b8a0ec63c128ed697d899dfc3f9ef
sczcz <funkheim@hotmail.com>
2023-04-21 12:13:43 +0200

    -Fixed pathway to links

--- sopa/src/main/java/com/sparaochpara/sop/service/TransactionService.java
+++
@@ -1,9 +0,0 @@
-package com.sparaochpara.sop.service;
-
-import com.sparaochpara.sop.dto.TransactionDto;
-
-import java.util.List;
-
-public interface TransactionService {
-    List<TransactionDto>findAllTransactions();
-}
