f76f2a5c32017c3f6d7745a3c10195a8b06e1b6a
sczcz <funkheim@hotmail.com>
2023-04-21 11:00:43 +0200

    Delete sopa 2 directory

--- sopa 2/src/main/java/com/sparaochpara/sop/service/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.sparaochpara.sop.service;
-
-import com.sparaochpara.sop.dto.CategoryDto;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<CategoryDto> findAllCategories();
-}
