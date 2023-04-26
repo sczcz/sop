91d058ca70ffbb163bbc8e9b07b979e2fb44db3b
maja <majaquick@hotmail.com>
2023-04-26 10:18:48 +0200

    added GroupController and created groupbyid method

--- src/main/java/com/sparaochpara/sop/service/GroupService.java
+++ src/main/java/com/sparaochpara/sop/service/GroupService.java
@@ -1,10 +1,12 @@
 package com.sparaochpara.sop.service;
 
 import com.sparaochpara.sop.dto.GroupDto;
+import com.sparaochpara.sop.dto.UserDto;
 
 import java.util.List;
 
 public interface GroupService {
 
     List<GroupDto> findAllGroups();
+    GroupDto findGroupById(Long id);
 }
