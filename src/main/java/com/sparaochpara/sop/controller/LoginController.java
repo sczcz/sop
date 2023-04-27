84b4b1654cf7fb287a3dac6af6d4d8151eda99bd
Heliasaradar <128679723+Heliasaradar@users.noreply.github.com>
2023-04-27 10:33:23 +0200

    skapade metod i LoginController

--- src/main/java/com/sparaochpara/sop/controller/LoginController.java
+++ src/main/java/com/sparaochpara/sop/controller/LoginController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
+import java.util.UUID;
+
 @Controller
 public class LoginController {
 
@@ -19,24 +22,32 @@ public class LoginController {
 
 
     @GetMapping("/login")
-    public String showLoginForm(){
+    public String showLoginForm() {
         return "index";
     }
 
     @PostMapping("/login")
-    public String processLoginForm(@RequestParam String email, @RequestParam String password, Model model){
+    public String processLoginForm(@RequestParam String email, @RequestParam String password, Model model, HttpSession session, RedirectAttributes redirectAttributes) {
         Optional<User> userOptional = userRepository.findById(email);
-        if(userOptional.isPresent()){
+        if (userOptional.isPresent()) {
             User user = userOptional.get();
-            if(user.getPassword().equals(password)){
+            if (user.getPassword().equals(password)) {
                 model.addAttribute("user", user);
+                session.setAttribute("user", user);
+                String sessionToken = generateSessionToken();
+                session.setAttribute("sessionToken", sessionToken);
+                redirectAttributes.addAttribute("sessionToken", sessionToken);
                 return "redirect:/users";
             }
         }
+        model.addAttribute("error", "Invalid Email or Password");
+        return "index";
 
-            model.addAttribute("error", "Invalid email or password");
-            return "index";
+    }
 
+
+    private String generateSessionToken() {
+        return UUID.randomUUID().toString();
     }
 
 
