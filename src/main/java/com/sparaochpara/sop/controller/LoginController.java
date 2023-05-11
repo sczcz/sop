e41fd2969a0220ef31a0880e1b13c1958b638d73
sczcz <funkheim@hotmail.com>
2023-05-11 09:06:09 +0200

    -Removed HttpSession from LoginController.

--- src/main/java/com/sparaochpara/sop/controller/LoginController.java
+++ src/main/java/com/sparaochpara/sop/controller/LoginController.java
@@ -8,11 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpSession;
 import java.util.Optional;
-import java.util.UUID;
 
 @Controller
 public class LoginController {
@@ -27,26 +23,16 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String processLoginForm(@RequestParam String email, @RequestParam String password, Model model, HttpSession session, RedirectAttributes redirectAttributes) {
+    public String processLoginForm(@RequestParam String email, @RequestParam String password, Model model) {
         Optional<User> userOptional = userRepository.findById(email);
         if (userOptional.isPresent()) {
             User user = userOptional.get();
             if (user.getPassword().equals(password)) {
                 model.addAttribute("user", user);
-                session.setAttribute("user", user);
-                String sessionToken = generateSessionToken();
-                session.setAttribute("sessionToken", sessionToken);
-                redirectAttributes.addAttribute("sessionToken", sessionToken);
-                return "redirect:/users";
+                return "redirect:/" + email + "/users";
             }
         }
         model.addAttribute("error", "Invalid Email or Password");
         return "index";
-
-    }
-
-
-    private String generateSessionToken() {
-        return UUID.randomUUID().toString();
     }
 }
